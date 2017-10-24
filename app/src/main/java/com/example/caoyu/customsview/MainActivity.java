6d95cfe57edc053860b44550005531b8650160d8
YuGoal <1127caoyu@gmail.com>
2017-10-24 17:31:34 +0800

    全屏

--- app/src/main/java/com/example/caoyu/customsview/MainActivity.java
+++ app/src/main/java/com/example/caoyu/customsview/MainActivity.java
@@ -13,9 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         /*set it to be no title*/
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-//       /*set it to be full screen*/
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       /*set it to be full screen*/
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//手机常亮
         setContentView(R.layout.activity_main);
     }
