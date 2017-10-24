dc13ded0dc525b4f565373180e19cb4252b1f3ac
YuGoal <1127caoyu@gmail.com>
2017-10-24 17:30:05 +0800

    时钟
    一款时钟应用

---
+++ app/src/main/java/com/example/caoyu/customsview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.caoyu.customsview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*set it to be no title*/
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//       /*set it to be full screen*/
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);//手机常亮
+        setContentView(R.layout.activity_main);
+    }
+
+}
